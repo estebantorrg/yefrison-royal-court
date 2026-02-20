23c0f2c6596d8a56b023b84162fff11bda10fb55
Esteban Torres <estebantorrg@gmail.com>
2026-02-20 03:15:53 -0500

    mensaje

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins:,
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
